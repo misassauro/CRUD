d67ed36ec20e0c5a5d0a501e02ff13149e61556b
zMSLz <misaelg.macedo@gmail.com>
2023-12-11 00:54:30 -0300

    adicionando data e hora do cadastro

--- index.php
+++ index.php
@@ -21,11 +21,12 @@ if(isset($_GET["mensagem"]) && !empty($_GET["mensagem"])) {
                     <th class="text-center">Foto</th>
                     <th class="text-center">Nome</th>
                     <th class="text-center">Quantidade</th> 
+                    <th class="text-center">Data de cadastro</th>
                     <th></th>
                 </tr>
                 <tbody>
                     <?php
-                        $query = "SELECT id, nome, qtde, imagem from produtos";
+                        $query = "SELECT id, nome, qtde, imagem, data_upload from produtos";
                         $dados = mysqli_query($conexao, $query);
 
                         if ($dados) {
@@ -37,6 +38,7 @@ if(isset($_GET["mensagem"]) && !empty($_GET["mensagem"])) {
                                 </td>
                                 <td class="text-center"><?php echo $linha["nome"];?></td>
                                 <td class="text-center"><?php echo $linha["qtde"];?></td>
+                                <td class="text-center"><?php echo date('d/m/Y H:i', strtotime($linha["data_upload"]));?></td>
                                 <td class="text-center">
                                     <a class="btn btn-warning" href="prodEditar.php?id=<?php echo $linha["id"];?>">Editar</a>
                                     <a class="btn btn-danger" href="prodExcluir.php?id=<?php echo $linha["id"];?>">Excluir</a>
